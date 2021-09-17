8d7590abc7acfb059f2951a059e77f646f875391
Axle75p <90444005+Axle75p@users.noreply.github.com>
2021-09-17 09:56:34 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.set('view engine', 'ejs');
+
+    
+
+
+app.get('/', (req, res) => {
+
+
+ 
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let today  = new Date();
+    let currentDate = today.toLocaleDateString("en-US", options);
+    console.log(currentDate);
+
+    res.render("index.ejs", {currentDate: currentDate});
+
+});
+
+
+/* app.post */
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
